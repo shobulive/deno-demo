00fa63213e953fd84bcca0a2b89c44927738011c
Shubham Singh <shubham.singh@airbnb.com>
2023-02-16 13:00:27 +0530

    Fix existing schema code

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
+import { v1 } from "https://deno.land/std/uuid/mod.ts";
 // @ts-ignore
 import { User } from "../models/entity.ts";
 // @ts-ignore
@@ -10,13 +10,13 @@ export /**
  *
  * @param {{ response: any }} { response }
  */
-const getAllUsers = ({ response }: { response: any }) => {
-  response.body = {
-    statusCode: 0,
-    statusMessage: "Yayyy! Success",
-    data: getUsers(),
+  const getAllUsers = ({ response }: { response: any }) => {
+    response.body = {
+      statusCode: 0,
+      statusMessage: "Yayyy! Success",
+      data: getUsers(),
+    };
   };
-};
 
 export /**
  * Gets user by ID
@@ -29,21 +29,21 @@ export /**
  *   response,
  * }
  */
-const getUserById = ({
-  params,
-  response,
-}: {
-  params: { id: string };
-  response: any;
-}) => {
-  const user: User | undefined = getUsers().find((p) => p.id === params.id);
-  const isSuccess = !!user;
-  response.body = {
-    statusCode: isSuccess ? 0 : 1,
-    statusMessage: isSuccess ? "Yayyy! Success" : "User not found",
-    data: user,
+  const getUserById = ({
+    params,
+    response,
+  }: {
+    params: { id: string };
+    response: any;
+  }) => {
+    const user: User | undefined = getUsers().find((p) => p.id === params.id);
+    const isSuccess = !!user;
+    response.body = {
+      statusCode: isSuccess ? 0 : 1,
+      statusMessage: isSuccess ? "Yayyy! Success" : "User not found",
+      data: user,
+    };
   };
-};
 
 export /**
  * This function adds a user
@@ -56,32 +56,32 @@ export /**
  *   response,
  * }
  */
-const addUser = async ({
-  request,
-  response,
-}: {
-  request: any;
-  response: any;
-}) => {
-  const body = await request.body();
-  if (!request.hasBody) {
-    response.status = 400;
-    response.body = {
-      statusCode: 1,
-      statusMessage: "Failure! No data provided",
-    };
-  } else {
-    const user: User = body.value;
-    user.id = v4.generate();
-    setUsers([...getUsers(), user]);
-    response.status = 201;
-    response.body = {
-      statusCode: 0,
-      statusMessage: "Yayyy! Success",
-      data: user,
-    };
-  }
-};
+  const addUser = async ({
+    request,
+    response,
+  }: {
+    request: any;
+    response: any;
+  }) => {
+    const body = request.body();
+    if (!request.hasBody) {
+      response.status = 400;
+      response.body = {
+        statusCode: 1,
+        statusMessage: "Failure! No data provided",
+      };
+    } else {
+      const user: User = await body.value;
+      user.id = v1.generate();
+      setUsers([...getUsers(), user]);
+      response.status = 201;
+      response.body = {
+        statusCode: 0,
+        statusMessage: "Yayyy! Success",
+        data: user,
+      };
+    }
+  };
 
 export /**
  * This function updates a user
@@ -97,38 +97,38 @@ export /**
  * }
  * @returns
  */
-const updateUser = async ({
-  params,
-  request,
-  response,
-}: {
-  params: { id: string };
-  request: any;
-  response: any;
-}) => {
-  const user: User | undefined = getUsers().find((p) => p.id === params.id);
-  if (!user) {
-    response.status = 404;
-    response.body = {
-      statusCode: 1,
-      statusMessage: "Failure! User Not Found",
-    };
-    return;
-  }
-  const body = await request.body();
+  const updateUser = async ({
+    params,
+    request,
+    response,
+  }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  }) => {
+    const user: User | undefined = getUsers().find((p) => p.id === params.id);
+    if (!user) {
+      response.status = 404;
+      response.body = {
+        statusCode: 1,
+        statusMessage: "Failure! User Not Found",
+      };
+      return;
+    }
+    const body = request.body();
 
-  const updateData: { name?: string; description?: string; price?: number } =
-    body.value;
-  setUsers(
-    getUsers().map((p) => (p.id === params.id ? { ...p, ...updateData } : p))
-  );
-  response.status = 200;
-  response.body = {
-    statusCode: 0,
-    statusMessage: "Yayyy! Success",
-    data: getUsers(),
+    const updateData: { name?: string; description?: string; } =
+      await body.value;
+    setUsers(
+      getUsers().map((p) => (p.id === params.id ? { ...p, ...updateData } : p))
+    );
+    response.status = 200;
+    response.body = {
+      statusCode: 0,
+      statusMessage: "Yayyy! Success",
+      data: getUsers(),
+    };
   };
-};
 
 export /**
  * This function deletes the user
@@ -142,24 +142,24 @@ export /**
  * }
  * @returns
  */
-const deleteUser = ({
-  params,
-  response,
-}: {
-  params: { id: string };
-  response: any;
-}) => {
-  if (!getUsers().find((u) => u.id == params.id)) {
-    response.status = 404;
+  const deleteUser = ({
+    params,
+    response,
+  }: {
+    params: { id: string };
+    response: any;
+  }) => {
+    if (!getUsers().find((u) => u.id == params.id)) {
+      response.status = 404;
+      response.body = {
+        statusCode: 1,
+        statusMessage: "Failure! User Not Found",
+      };
+      return;
+    }
+    setUsers(getUsers().filter((p) => p.id !== params.id));
     response.body = {
-      statusCode: 1,
-      statusMessage: "Failure! User Not Found",
+      statusCode: 0,
+      statusMessage: "Yayyy! Success",
     };
-    return;
-  }
-  setUsers(getUsers().filter((p) => p.id !== params.id));
-  response.body = {
-    statusCode: 0,
-    statusMessage: "Yayyy! Success",
   };
-};
